3c3b211fe839562f4841f29165ac86dbe96ba82c
Artem D <artem@daniliants.com>
2021-04-19 01:13:26 +0300

    Removed hamburger menu and foot notice

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 st.title('Hello world!')
