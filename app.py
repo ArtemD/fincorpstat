38a952fc28b7c38e23a4b8a0a8b99da9d36a05cc
Artem D <artem@daniliants.com>
2021-04-19 01:35:58 +0300

    Minor change

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ import pandas as pd
 
 hide_streamlit_style = """
 <style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
+#MainMenu {
+    visibility: hidden;
+}
+footer {
+    visibility: hidden;
+}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
